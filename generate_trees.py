b4ab84285d04e3ac64f2c32d2bab850f7b0e929d
kawilliams <kawilliams@email.arizona.edu>
2018-02-18 12:29:14 -0700

    Add command line args to tree.py call

---
+++ generate_trees.py
@@ -0,0 +1,12 @@
+#Write index.html. Need compressed_tree.html, reingold_tilford.html
+
+f = open('custom_reingold.html', 'w')
+
+message = """<html>
+<head></head>
+<body><p>Hello katy generate trees</p><body>
+</html>"""
+
+f.write(message)
+f.close()
+
